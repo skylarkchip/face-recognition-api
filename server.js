725ec43795f3dda8247a16ec4dd0c35dc9d3c7f9
Mark Marasigan <kramlivingstone@gmail.com>
2020-07-16 19:15:35 +0800

    Updated server.js

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleClarifaiApi(req, res) });
 
 
-app.listen(3000, () => {
-    console.log('App Running on Port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App is running on port ${process.env.PORT}`);
 });
 
 
